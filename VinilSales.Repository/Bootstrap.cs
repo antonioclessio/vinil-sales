986a1f965ba0c835085746852758573329806139
Antônio Cléssio <antonio.clessio@swfast.com.br>
2019-06-10 17:30:52 -0300

    Adicionando AlbumDbContext ao contexto principal.

--- VinilSales.Repository/Bootstrap.cs
+++ VinilSales.Repository/Bootstrap.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using VinilSales.Repository.AlbumContext.DbContexts;
 using VinilSales.Repository.ClienteContext.DbContexts;
 
 namespace VinilSales.Repository
 {
     public static class Bootstrap
     {
+        private const string DatabaseName = "VinilSales";
+
         public static void ConfigureDependencyInjection(ref IServiceCollection services)
         {
-            services.AddDbContext<ClienteDbContext>();
+            services.AddDbContext<AlbumDbContext>(opt => opt.UseInMemoryDatabase(DatabaseName));
+            services.AddDbContext<ClienteDbContext>(opt => opt.UseInMemoryDatabase(DatabaseName));
         }
     }
 }
