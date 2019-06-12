7e3fbb896dd7798582d9d66286cd718b2b08e338
Antônio Cléssio <clessio@outlook.com>
2019-06-11 21:09:32 -0300

    Evoluindo os contextos do cliente.

--- VinilSales.Application/ClienteContext/Results/GetClientesResult.cs
+++ VinilSales.Application/ClienteContext/Results/GetClientesResult.cs
@@ -1,10 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using VinilSales.Domain.CoreContext.ValueObjects;
 
 namespace VinilSales.Application.ClienteContext.Results
 {
     public class GetClientesResult
     {
+        public int IdCliente { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataHoraCadastro { get; set; }
+        public short Status { get; set; }
+        public CPF CPF { get; set; }
     }
 }
